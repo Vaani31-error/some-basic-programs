ad24cb027669f5a83f5c86f0e4ecffeb94ac264a
Vani Sharma <sharmavaani219@gmail.com>
2024-08-01 13:08:36 +0530

    square

---
+++ squaree.py
@@ -0,0 +1,6 @@
+#Write a program that will take three digits from the user and add the square of each digit.
+a=int(input("enter the value: "))
+b=int(input("enter the value: "))
+c=int(input("enter the value: "))
+final_value=a*a+b*b+c*c
+print(final_value)
